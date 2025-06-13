86f304c1d851ef836bc290871f39d497749447e1
Giacomo GK <giacomoguiulfo@users.noreply.github.com>
2025-06-13 09:14:04 +0900

    Add model tests for Gist and Gist::File

--- test/models/gist_test.rb
+++ test/models/gist_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class GistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "languages returns up to three capitalized languages" do
+    user = User.create!(username: "tester", email: "tester@example.com")
+    gist = Gist.new(user: user)
+    gist.files.build(filename: "a.rb", language: "ruby", content: "puts")
+    gist.files.build(filename: "b.rb", language: "ruby", content: "puts")
+    gist.files.build(filename: "c.py", language: "python", content: "print")
+    gist.files.build(filename: "d.js", language: "javascript", content: "console.log")
+    gist.save!
+
+    assert_equal ["Ruby", "Python", "Javascript"], gist.languages
+  end
 end
